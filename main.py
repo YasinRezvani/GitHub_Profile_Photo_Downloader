b0cf61b3203db4cd6319404a8da4c117b8b132d4
YasinRezvani <yasinrezvani2025@gmail.com>
2021-07-30 00:47:43 +0430

    requested the url git hub and use beautifulsoup

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 ﻿import requests 
 from bs4 import BeautifulSoup as bs
 
+github_user = input("Enter GitHub user: ")
+url = "https://github.com/" + github_user
+req = requests.get(url)
+scrab = bs(req.content, "html.parser")
+data = scrab.find("img" , {"alt" : "Avatar"})['src']
+print(data)
+
+# Made By Yasin Rezvani
+
