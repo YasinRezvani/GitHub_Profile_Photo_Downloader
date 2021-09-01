ec1506e825d66964933bc7697449a0c4b25e33f9
YasinRezvani <yasinrezvani2025@gmail.com>
2021-09-01 20:44:38 +0430

    change name and change the a input()

---
+++ GitHub_Profile_Photo_Downloader.py
@@ -0,0 +1,18 @@
+﻿import requests 
+from bs4 import BeautifulSoup as bs
+from beautifultable import BeautifulTable
+
+table = BeautifulTable()
+github_user = input("Enter GitHub Username: ")
+url = "https://github.com/" + github_user
+req = requests.get(url)
+scrab = bs(req.content, "html.parser")
+data = scrab.find("img" , {"alt" : "Avatar"})['src']
+table.rows.append([data])                
+table.columns.header = ["Download Link"]
+table.set_style(BeautifulTable.STYLE_BOX_ROUNDED) 
+print(table)
+input()
+
+# Made By Yasin Rezvani
+
