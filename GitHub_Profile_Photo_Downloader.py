6d004ba6896739af4b186053bd4a19c8f73b02aa
YasinRezvani <yasinrezvani2025@gmail.com>
2021-09-04 21:43:02 +0430

    add auto copy link to clipboard with os module

--- GitHub_Profile_Photo_Downloader.py
+++ GitHub_Profile_Photo_Downloader.py
@@ -1,15 +1,20 @@
 ﻿import requests 
 from bs4 import BeautifulSoup as bs
 from beautifultable import BeautifulTable
+import os
 
 table = BeautifulTable()
-github_user = input("Enter GitHub Username: ")
+github_user = input("\nEnter GitHub Username: ")
 url = "https://github.com/" + github_user
 req = requests.get(url)
 scrab = bs(req.content, "html.parser")
 data = scrab.find("img" , {"alt" : "Avatar"})['src']
+print("\n")
 table.rows.append([data])                
+copy_link = "echo " + data.strip() + "| clip"
+os.system(copy_link)
 table.columns.header = ["Download Link"]
+table.rows.append(["Download Link Copy in Clipboard Successfully"])
 table.set_style(BeautifulTable.STYLE_BOX_ROUNDED) 
 print(table)
 input()
