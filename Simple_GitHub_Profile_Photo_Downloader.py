1dc2529066d79eff03901c61264786f9a1db61ba
YasinRezvani <yasinrezvani2025@gmail.com>
2021-09-15 10:17:24 +0430

    add (try except) to prevent error

--- Simple_GitHub_Profile_Photo_Downloader.py
+++ Simple_GitHub_Profile_Photo_Downloader.py
@@ -3,21 +3,27 @@ from bs4 import BeautifulSoup as bs
 from beautifultable import BeautifulTable
 import os
 
-table = BeautifulTable()
-github_user = input("\nEnter GitHub Username: ")
-url = "https://github.com/" + github_user
-req = requests.get(url)
-scrab = bs(req.content, "html.parser")
-data = scrab.find("img" , {"alt" : "Avatar"})['src']
-print("")
-table.rows.append([data])                
-copy_link = "echo " + data.strip() + "| clip"
-os.system(copy_link)
-table.columns.header = ["Download Link"]
-table.rows.append(["Download Link Copy in Clipboard Successfully"])
-table.set_style(BeautifulTable.STYLE_BOX_ROUNDED) 
-print(table)
-input()
+try:
+ table = BeautifulTable()
+ github_user = input("\nEnter GitHub Username: ")
+ url = "https://github.com/" + github_user
+ req = requests.get(url)
+ scrab = bs(req.content, "html.parser")
+ data = scrab.find("img" , {"alt" : "Avatar"})['src']
+ print("")
+ table.rows.append([data])                
+ copy_link = "echo " + data.strip() + "| clip"
+ os.system(copy_link)
+ table.columns.header = ["Download Link"]
+ table.rows.append(["Download Link Copy in Clipboard Successfully"])
+ table.set_style(BeautifulTable.STYLE_BOX_ROUNDED) 
+ print(table)
+ input()
+ 
+except:
+ print("")
+ print("--Username Not Found , Invalid Mode--")
+ input()
 
 # Made By Yasin Rezvani
 
